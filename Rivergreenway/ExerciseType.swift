f0907bdb2b429c76840e6de9d99ac1b23a7d3f15
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-01-20 22:06:31 -0500

    added exercise type enum

---
+++ Rivergreenway/ExerciseType.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseType.swift
+//  Rivergreenway
+//
+//  Created by Scott Weidenkopf on 1/20/16.
+//  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import Foundation
+
+enum ExerciseType: Double {
+    case BIKING = 8.00
+    case WALKING = 3.80
+    case RUNNING = 7.50
+    case SKATEBOARDING = 6.50
+    
+}
