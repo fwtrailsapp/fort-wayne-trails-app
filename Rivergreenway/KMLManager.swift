59bef249cd8224f93fadf56eab05ee5b4655a5b0
libjared <libjared@users.noreply.github.com>
2016-01-24 17:52:09 -0500

    add kmlmanager, tools, tests for both

---
+++ Rivergreenway/KMLManager.swift
@@ -0,0 +1,9 @@
+//
+//  KMLManager.swift
+//  Rivergreenway
+//
+//  Created by Jared P on 1/24/16.
+//  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import Foundation
