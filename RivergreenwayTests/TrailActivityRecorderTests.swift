2e491b0b5091e1d51a3a89243d49556722c47c10
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-01-29 18:26:46 -0500

    Create TrailActivityRecorder class and tests

---
+++ RivergreenwayTests/TrailActivityRecorderTests.swift
@@ -0,0 +1,9 @@
+//
+//  TrailActivityRecorderTests.swift
+//  Rivergreenway
+//
+//  Created by Scott Weidenkopf on 1/29/16.
+//  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import Foundation
