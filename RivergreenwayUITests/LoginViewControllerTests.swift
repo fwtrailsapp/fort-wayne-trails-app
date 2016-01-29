9d6c82728fadc508d4fa54c8ed54147e050ef2ee
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-01-29 18:32:42 -0500

    Updated tests

--- RivergreenwayUITests/LoginViewControllerTests.swift
+++ RivergreenwayUITests/LoginViewControllerTests.swift
@@ -2,7 +2,7 @@
 //  LoginViewControllerTests.swift
 //  Rivergreenway
 //
-//  Created by Scott Weidenkopf on 1/24/16.
+//  Created by Scott Weidenkopf on 1/29/16.
 //  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
 //
 
@@ -10,12 +10,35 @@ import XCTest
 
 class LoginViewControllerTests: XCTestCase {
     
+    var app: XCUIApplication?
+    
     override func setUp() {
         super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        app = XCUIApplication()
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        app!.launch()
+        
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
+    
+    func testLoginButton() {
+        app!.buttons["Login"].tap()
+        
+        
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
 }
