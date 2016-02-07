99f7446d8c17ed9d63a7309f6c42a1972e3a0e12
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-02-06 19:50:53 -0500

    begun implementing create account view and controller

---
+++ Rivergreenway/CreateAccountViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CreateAccountViewController.swift
+//  Rivergreenway
+//
+//  Created by Scott Weidenkopf on 2/6/16.
+//  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UITableViewController {
+
+    @IBOutlet weak var dateTextField: UITextField!
+        
+    @IBAction func didBeginEditingDateTextField(sender: UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func datePickerValueChanged(sender:UIDatePicker) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        dateTextField.text = dateFormatter.stringFromDate(sender.date)
+    }
+}
