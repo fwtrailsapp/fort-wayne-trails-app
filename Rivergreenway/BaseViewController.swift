c22ae71210928ac947d399e9f451a16a2057cfe8
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-02-09 20:43:23 -0500

    Added base functionality for closing keyboards on screen touch

---
+++ Rivergreenway/BaseViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BaseViewController.swift
+//  Rivergreenway
+//
+//  Created by Scott Weidenkopf on 2/9/16.
+//  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+}
