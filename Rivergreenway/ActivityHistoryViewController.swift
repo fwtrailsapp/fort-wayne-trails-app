229bfe2f13bded7ac14e2fdaba2beb5f667e0a3d
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-02-10 22:10:30 -0500

    added ActivityHistoryViewController

---
+++ Rivergreenway/ActivityHistoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityHistoryViewController.swift
+//  Rivergreenway
+//
+//  Created by Scott Weidenkopf on 2/10/16.
+//  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import UIKit
+
+class ActivityHistoryViewController: DraweredViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
