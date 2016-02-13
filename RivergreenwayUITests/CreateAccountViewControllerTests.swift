a2e62a51a59f0dc93429a917b65efcb0fcdaf44e
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-02-13 12:40:22 -0500

    create account view controller test setup

--- RivergreenwayUITests/CreateAccountViewControllerTests.swift
+++ RivergreenwayUITests/CreateAccountViewControllerTests.swift
@@ -10,11 +10,15 @@ import XCTest
 
 class CreateAccountViewControllerTests: XCTestCase {
     
+    var app: XCUIApplication?
+    var tablesQuery: XCUIElementQuery?
+    
     override func setUp() {
         super.setUp()
        
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+        app = XCUIApplication()
+        tablesQuery = app!.tables
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
