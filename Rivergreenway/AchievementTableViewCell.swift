e7cb8e3b7cf35963539eaa56916428832151d567
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-02-13 19:42:04 -0500

    added achievement view controller

---
+++ Rivergreenway/AchievementTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AchievementTableViewCell.swift
+//  Rivergreenway
+//
+//  Created by Scott Weidenkopf on 2/13/16.
+//  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import UIKit
+
+class AchievementTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
