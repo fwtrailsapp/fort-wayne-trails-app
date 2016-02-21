a031b4f752351549895950c499ecfd7292135d98
libjared <libjared@users.noreply.github.com>
2016-02-20 20:49:01 -0500

    add webstore and tests

---
+++ Rivergreenway/WebStore.swift
@@ -0,0 +1,10 @@
+//
+//  WebStore.swift
+//  Rivergreenway
+//
+//  Created by Jared P on 2/20/16.
+//  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import Foundation
+
---
+++ RivergreenwayTests/WebStoreTests.swift
@@ -0,0 +1,27 @@
+//
+//  WebStoreTests.swift
+//  Rivergreenway
+//
+//  Created by Jared P on 2/20/16.
+//  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import XCTest
+
+class WebStoreTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
