a41cf643261ddfda8530510bb7b3ecc5d9bb336e
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-02-23 22:40:48 -0500

    date text field now has year picker instead of keyboard

--- Rivergreenway/CreateAccountViewController.swift
+++ Rivergreenway/CreateAccountViewController.swift
@@ -8,21 +8,46 @@
 
 import UIKit
 
-class CreateAccountViewController: BaseTableViewController {
+class CreateAccountViewController: BaseTableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var dateTextField: UITextField!
+    private var years: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Year], fromDate: date)
+        
+        let currYear =  Int(components.year)
+        
+        for year in 1900...currYear {
+            years.append(String(year))
+        }
+    }
         
     @IBAction func didBeginEditingDateTextField(sender: UITextField) {
-        let datePickerView:UIDatePicker = UIDatePicker()
-        datePickerView.datePickerMode = UIDatePickerMode.Date
-        sender.inputView = datePickerView
-        datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+        let yearPickerView:UIPickerView = UIPickerView()
+        yearPickerView.dataSource = self
+        yearPickerView.delegate = self
+        sender.inputView = yearPickerView
     }
     
-    func datePickerValueChanged(sender:UIDatePicker) {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
-        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
-        dateTextField.text = dateFormatter.stringFromDate(sender.date)
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return years.count;
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return years[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        dateTextField.text = years[row]
     }
 }
