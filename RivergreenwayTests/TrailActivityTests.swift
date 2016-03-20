0b97187811192b126aa009d6a0deb4936928fb35
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-03-20 16:11:50 -0400

    added distance and average speed tests for trailactivity

--- RivergreenwayTests/TrailActivityTests.swift
+++ RivergreenwayTests/TrailActivityTests.swift
@@ -31,6 +31,8 @@ class TrailActivityTests : XCTestCase {
     func testDistance() {
         let distanceTraveled = emptyActivity!.getDistance()
         assert(distanceTraveled == 0)
+        
+        XCTAssertEqualWithAccuracy(basicActivity!.getDistance(), 1264 / 5280, accuracy: 0.05)
     }
     
     func testDuration() {
@@ -41,6 +43,7 @@ class TrailActivityTests : XCTestCase {
     func testAverageSpeed() {
         let averageSpeed = emptyActivity!.getAverageSpeed()
         assert(averageSpeed == 0)
+        XCTAssertEqualWithAccuracy(basicActivity!.getAverageSpeed(), 14.36364, accuracy: 0.05)
     }
     
     func initializeEmptyActivity() {
@@ -54,10 +57,18 @@ class TrailActivityTests : XCTestCase {
     
     func initializeBasicActivity() {
         let startTime = NSDate()
-        let duration: Double = 60000 // add a minute
+        let duration: Double = 60 // add a minute
         let exerciseType = ExerciseType.Run
         let caloriesBurned: Double = 1000
-        let path = [GMSMutablePath]()
-        self.basicActivity = TrailActivity(startTime: startTime, duration: duration, distance: 0, path: path, exerciseType: exerciseType, caloriesBurned: caloriesBurned)
+        let coord1 = CLLocationCoordinate2D(latitude: -85.189383,longitude: 40.9806756)
+        let coord2 = CLLocationCoordinate2D(latitude: -85.1909924,longitude: 40.9795255)
+        let coord3 = CLLocationCoordinate2D(latitude: -85.1926017,longitude: 40.9782133)
+        var path = [GMSMutablePath]()
+        let path1 = GMSMutablePath()
+        path1.addCoordinate(coord1)
+        path1.addCoordinate(coord2)
+        path1.addCoordinate(coord3)
+        path.append(path1)
+        self.basicActivity = TrailActivity(startTime: startTime, duration: duration, distance: 1264/5280, path: path, exerciseType: exerciseType, caloriesBurned: caloriesBurned)
     }
 }
