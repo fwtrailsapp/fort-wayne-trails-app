7faecfd58b459502eac71801b50294bec2ad4341
libjared <libjared@users.noreply.github.com>
2016-03-23 20:55:40 -0400

    trailactivityhistoryresponse

---
+++ Rivergreenway/TrailActivityHistoryResponse.swift
@@ -0,0 +1,30 @@
+//
+//  TrailActivityHistoryResponse.swift
+//  Rivergreenway
+//
+//  Created by Jared P on 3/20/16.
+//  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import Foundation
+import JSONJoy
+
+struct TrailActivityHistoryResponse : JSONJoy {
+    let activities: [TrailActivity]
+    
+    init(_ decoder: JSONDecoder) throws {
+        guard let ra = decoder["GetActivitiesForUserResult"].array else {
+            throw JSONError.WrongType
+        }
+        
+        var acts = [TrailActivity]()
+        for item in ra {
+            guard let thisActivity = try? TrailActivity(item) else {
+                throw JSONError.WrongType
+            }
+            acts.append(thisActivity)
+        }
+        
+        self.activities = acts
+    }
+}
