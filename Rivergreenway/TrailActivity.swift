2ac16856f04c24883706883ac99fe16b9ad7e1d5
libjared <libjared@users.noreply.github.com>
2016-03-23 21:01:00 -0400

    trailactivity init from json

--- Rivergreenway/TrailActivity.swift
+++ Rivergreenway/TrailActivity.swift
@@ -7,23 +7,48 @@
 //
 
 import Foundation
+import JSONJoy
 
 class TrailActivity {
     
     private var startTime: NSDate
     private var duration: NSTimeInterval // seconds
-    private var path: [GMSMutablePath]
+    private var path: [GMSMutablePath]?
     private var exerciseType: ExerciseType
     private var caloriesBurned: Double
     private var distance: Double // miles
     
-    init(startTime: NSDate, duration: NSTimeInterval, distance: Double, path: [GMSMutablePath], exerciseType: ExerciseType, caloriesBurned: Double) {
+    init(startTime: NSDate, duration: NSTimeInterval, distance: Double, path: [GMSMutablePath]? = nil, exerciseType: ExerciseType, caloriesBurned: Double) {
         self.startTime = startTime
         self.duration = duration
         self.exerciseType = exerciseType
         self.caloriesBurned = caloriesBurned
         self.path = path
         self.distance = distance
+    }
+    
+    init(_ decoder: JSONDecoder) throws {
+        let uTimeStarted: String = try decoder["time_started"].getString()
+        let uDuration: String = try decoder["duration"].getString()
+        let uExType: String = try decoder["exercise_type"].getString()
+        let uCalories: Double = try decoder["calories_burned"].getDouble()
+        let uMileage: Double = try decoder["mileage"].getDouble()
+        
+        let oStartTime: NSDate? = Converter.stringToDate(uTimeStarted)
+        let oDuration: NSTimeInterval? = Converter.stringToTimeInterval(uDuration)
+        let oExType: ExerciseType? = ExerciseType.fromStringIgnoreCase(uExType)
+        let oCalories: Double? = uCalories
+        let oMileage: Double? = uMileage
+        
+        if oStartTime == nil || oDuration == nil || oExType == nil || oCalories == nil || oMileage == nil {
+            throw JSONError.WrongType
+        }
+        
+        self.startTime = oStartTime!
+        self.duration = oDuration!
+        self.exerciseType = oExType!
+        self.caloriesBurned = oCalories!
+        self.distance = oMileage!
     }
     
     // returns distance in miles
@@ -47,7 +72,7 @@ class TrailActivity {
         return exerciseType
     }
     
-    func getPath() -> [GMSMutablePath] {
+    func getPath() -> [GMSMutablePath]? {
         return path
     }
     
