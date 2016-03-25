7f3c900c6d22d7b08d11aeab761e1dd7d08581fc
libjared <libjared@users.noreply.github.com>
2016-03-24 23:55:32 -0400

    userstatisticsresponse

---
+++ Rivergreenway/UserStatisticsResponse.swift
@@ -0,0 +1,57 @@
+//
+//  UserStatisticsResponse.swift
+//  Rivergreenway
+//
+//  Created by Jared P on 3/24/16.
+//  Copyright © 2016 City of Fort Wayne Rivergreenways. All rights reserved.
+//
+
+import Foundation
+import JSONJoy
+
+struct UserStatisticsResponse : JSONJoy {
+    let stats: [SingleStatistic]
+    
+    init(_ decoder: JSONDecoder) throws {
+        guard let ra = decoder["GetTotalStatsForUserResult"].array else {
+            throw JSONError.WrongType
+        }
+        
+        var statsArray = [SingleStatistic]()
+        for item in ra {
+            guard let thisStatistic = try? SingleStatistic(item) else {
+                throw JSONError.WrongType
+            }
+            statsArray.append(thisStatistic)
+        }
+        self.stats = statsArray
+    }
+}
+
+struct SingleStatistic : JSONJoy {
+    let calories: Int
+    let distance: Double
+    let duration: NSTimeInterval
+    let type: ExerciseType
+    
+    init(_ decoder: JSONDecoder) throws {
+        let uCalories = try decoder["total_calories"].getInt()
+        let uDistance = try decoder["total_distance"].getDouble()
+        let uDuration = try decoder["total_duration"].getString()
+        let uType = try decoder["type"].getString()
+        
+        let oCalories: Int? = uCalories
+        let oDistance: Double? = uDistance
+        let oDuration: NSTimeInterval? = Converter.stringToTimeInterval(uDuration)
+        let oType: ExerciseType? = ExerciseType.fromStringIgnoreCase(uType)
+        
+        if oCalories == nil && oDistance == nil && oDuration == nil && oType == nil {
+            throw JSONError.WrongType
+        }
+        
+        self.calories = oCalories!
+        self.distance = oDistance!
+        self.duration = oDuration!
+        self.type = oType!
+    }
+}
