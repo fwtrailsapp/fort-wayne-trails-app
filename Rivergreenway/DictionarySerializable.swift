5085e14d24ff7cec75cf65b280377ee060c32ad1
Scott Weidenkopf <sweidenkopf@gmail.com>
2016-04-03 19:19:51 -0400

    added dictionary serializable protocol

---
+++ Rivergreenway/DictionarySerializable.swift
@@ -0,0 +1,17 @@
+//
+//  DictionarySerializable.swift
+//  Rivergreenway
+//
+//  Created by Scott Weidenkopf on 4/3/16.
+//  Copyright © 2016 City of Fort Wayne Greenways and Trails Department. All rights reserved.
+//
+
+import Foundation
+
+/**
+ SwiftHTTP can serialize Swift dictionaries into JSON automatically. Our models
+ that implement this protocol thus can be serialized into JSON automatically.
+ */
+protocol DictionarySerializable {
+    func toDictionary() -> [String: AnyObject?]
+}
