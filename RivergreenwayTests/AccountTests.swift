78786512ac9b31dd4d46ffdae0fd31fc96a07a77
libjared <libjared@users.noreply.github.com>
2016-04-16 00:24:41 -0400

    accounttests: test (de)serialize

--- RivergreenwayTests/AccountTests.swift
+++ RivergreenwayTests/AccountTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 @testable import Rivergreenway
+import JSONJoy
 
 class AccountTests: XCTestCase {
     
@@ -41,5 +42,19 @@ class AccountTests: XCTestCase {
         print(scottBMR!)
         print(scottAccount!.getAge()!)
         XCTAssertEqualWithAccuracy(scottBMR!, 1683, accuracy: 50.0)
+    }
+    
+    //try to encode account as JSON then immediately decode it back
+    func testSerialize() {
+        let dec = JSONDecoder(emptyAccount!.toDictionary())
+        if dec["sex"].rawValue! as! NSObject != NSNull() {
+            XCTFail()
+        }
+        let acctBack = try! Account(dec, username: emptyAccount!.username)
+        XCTAssertEqual(emptyAccount!.username, acctBack.username)
+        XCTAssertEqual(emptyAccount!.birthYear, acctBack.birthYear)
+        XCTAssertEqual(emptyAccount!.weight, acctBack.weight)
+        XCTAssertEqual(emptyAccount!.sex, acctBack.sex)
+        XCTAssertEqual(emptyAccount!.height, acctBack.height)
     }
 }
